f49eaf690edc386a093068dc8310d1e342b0e398
cyrus0707 <cyrusmarkl05@gmail.com>
2025-05-27 16:42:29 +0100

    loop continued

--- StarterPack/main.cpp
+++ StarterPack/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	int iScore = 
+	int iScore = 0;
 
 	char cIn;
 
--- Week 2 - Loop Sandbox/main.cpp
+++ Week 2 - Loop Sandbox/main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
-
+	int count;
+	count = 0;
+	while (count < 100)
+	{
+		cout << count << endl;
+		count++;
+	}
+	cout << "Done" << endl;
+	return 0;
 	return 0;
 }
