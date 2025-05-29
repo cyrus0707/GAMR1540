a2b8f28a03e156334a65319c77b37ae862cda21f
cyrus0707 <cyrusmarkl05@gmail.com>
2025-05-29 03:28:05 +0100

    loop experiment

--- Week 2 - Loop Sandbox/main.cpp
+++ Week 2 - Loop Sandbox/main.cpp
@@ -2,15 +2,67 @@
 
 using namespace std;
 
+float fNumber = 5.0f;
+
+float square()
+{
+	float result;
+	result = fNumber * fNumber;
+	return result;
+}
+
 int main()
 {
-	cout << "i\tj" << endl;
+	float fAnotherNumber;
+	fAnotherNumber = square();
+	cout << fAnotherNumber << endl;
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+
+/**string row1 = " ** ** ** ** ** **";
+string row2 = "** ** ** ** ** ** ";
+for (int i = 0; i < 8; i++)
+{
+	if (i % 2)
+	{
+		cout << row2 << endl; // Odd row
+	}
+	else
+	{
+		cout << row1 << endl; // Even row
+	}
+}
+
+/**string row1 = " ** ** ** ** ** **";
+string row2 = "** ** ** ** ** ** ";
+for (int i = 0; i < 6; i++)
+{
+	cout << row1 << endl;
+	cout << row2 << endl;
+/
+
+
+
+
+
+
+/**cout << "i\tj" << endl;
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
 			cout << i << "\t" << j << endl;
 		}
+	}
+
+		return 0;
 
 }
-                 
+                 *///
