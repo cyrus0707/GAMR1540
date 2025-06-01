05179ba24cabe6df36960e30aa17f8f6cbe1fc43
cyrus0707 <cyrusmarkl05@gmail.com>
2025-06-01 06:57:27 +0100

    unc

--- Week 2 - Loop Sandbox/main.cpp
+++ Week 2 - Loop Sandbox/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+
 using namespace std;
 
 float fNumber = 5.0f;
@@ -11,7 +12,9 @@ float square()
 	return result;
 }
 
+
 int main()
+
 {
 	float fAnotherNumber;
 	fAnotherNumber = square();
@@ -19,11 +22,26 @@ int main()
 	system("pause");
 	return 0;
 }
+	
+/*
+char cChars[10];
+for (int i = 0; i < 10; i++)
+{
+	cout << "Enter character number " << i + 1 << ": "; cin >> cChars[i];
+}
 
+// 10 times to multiplication grid
+/*
+	for (int i = 1; i <= 10; ++i) {
+		// Inner loop for columns
+		for (int j = 1; j <= 10; ++j) {
+			cout << i * j << "\t";
+		}
+		cout << endl; // New line after each row
+	}
 
-
-
-
+	return 0;
+	*/
 
 
 /**string row1 = " ** ** ** ** ** **";
